bc8af9249d32dbf20c97cdd811d4cb02ceb9c01d
Mehdi Hajikhani <mhaji007@fiu.edu>
2022-11-05 18:29:14 -0800

    Implement hashPassword and comparePassword util functions

---
+++ utils/auth.js
@@ -0,0 +1,25 @@
+// Houses auth related utility functions
+
+import bcrypt from "bcrypt";
+
+export const hashPassword = (password) => {
+  return new Promise((resolve, reject) => {
+    bcrypt.genSalt(12, (err, salt) => {
+      if (err) {
+        reject(err);
+      }
+      bcrypt.hash(password, salt, (err, hash) => {
+        if (err) {
+          reject(err);
+        }
+        resolve(hash);
+      });
+    });
+  });
+};
+
+// password: from frontend
+// hashedPassword: from database
+export const comparePassword = (password, hashedPassword) => {
+  return bcrypt.compare(password, hashedPassword);
+};
