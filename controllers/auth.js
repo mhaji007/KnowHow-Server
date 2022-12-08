36249c6aaad0a5185e8581bcf20324984e12e405
Mehdi Hajikhani <mhaji007@fiu.edu>
2022-12-07 22:41:14 -0800

    Set up login route

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,7 +1,8 @@
 import User from "../models/user";
 import { hashPassword, comparePassword } from "../utils/auth";
+import jwt from "jsonwebtoken";
 
-export const register = async (req, res, next) => {
+export const register = async (req, res) => {
   try {
     const { name, email, password } = req.body;
 
@@ -16,10 +17,12 @@ export const register = async (req, res, next) => {
     let userExist = await User.findOne({ email }).exec();
     if (userExist) return res.status(400).send("Email is taken");
 
-    // hash password
+    // Hash password
+
     const hashedPassword = await hashPassword(password);
 
-    // register user
+    // Register user
+
     const user = new User({
       name,
       email,
@@ -32,6 +35,64 @@ export const register = async (req, res, next) => {
     return res.json({ ok: true });
   } catch (err) {
     console.error(err);
-    return res.status(400).send("Error. Try again");
+    return re.status(400).send("Error. Try again");
+  }
+};
+
+// Login
+
+// Check if user's password is correct.
+// Take user's password, hash it and compare it against
+// the already hashed password stored in the database.
+// Generate json web token / JWT and send it to client.
+// This JWT will be sent in as cookie headers and
+// allows users access to protected routes
+
+export const login = async (req, res) => {
+  try {
+    // console.log(req.body);
+    const { email, password } = req.body;
+
+    console.log("password: " + password);
+    console.log("email: " + email);
+
+    // Check if user exists in database
+    const user = await User.findOne({ email }).exec();
+    console.log("user" + user)
+    if (!user) res.status(404).send("No user found");
+
+    // Compare passwords
+    const match = await comparePassword(password, user.password);
+
+    console.log("match====", match);
+    if (!match) {
+      return res.status(400).send("Error. Try again");
+    } else {
+      // Create signed token
+      // First argument is the data to include in the signed token
+      // (e.g., here user id is being included).
+      // Once the token is verified later, we 
+      // will have access to user id as well.
+      const token = jwt.sign({ _id: user._id }, process.env.JWT_SECRET, {
+        expiresIn: process.env.JWT_EXPIRATION_TIME,
+      });
+
+      // Return user and token to client, excluding the hashed password
+      user.password = undefined;
+
+      res.cookie("token", token, {
+        httpOnly: true,
+
+        // Only works on https - for production
+        // once we have https with ssl certificate
+        // secure: true
+      });
+
+      //  Send user as json response
+      res.json(user);
+    }
+  } catch (err) {
+    console.log(err);
+    return res.status(400).send("Error. Try again.");
   }
 };
--- routes/auth.js
+++ routes/auth.js
@@ -1,15 +1,13 @@
 import express from "express";
 const router = express.Router();
 // Controllers
-import {register} from "../controllers/auth"
+import { register, login } from "../controllers/auth";
 
 router.post("/register", register);
-
-
+router.post("/login", login);
 
 // Import validators
 
 // Import controllers
-
 
 module.exports = router;
