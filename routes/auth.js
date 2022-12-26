1198407101f795bfb2fb8cb55f1130000fdcc52e
Mehdi Hajikhani <mhaji007@fiu.edu>
2022-12-25 19:23:10 -0800

    Adjust forgot password route

--- routes/auth.js
+++ routes/auth.js
@@ -19,6 +19,6 @@ router.post("/register", register);
 router.post("/login", login);
 router.get("/logout", logout);
 router.get("/current-user", requireSignin, currentUser);
-router.get("/send-email", sendEmail);
+router.post("/forgot-password", sendEmail);
 
 module.exports = router;
